205ceda994c88f5baed163d2ce21a57fa10d0520
MartinChavez <info@martinchavezaguilar.com>
2015-11-17 19:37:55 -0800

    Add event definition and example

---
+++ 2 - Events.js
@@ -0,0 +1,31 @@
+/* Events
+
+- Similar to how the DOM works, it triggers events and handles the callback functions
+- Many objects in Node emit events, in general, these are inherited from the EventEmitter constructor
+*/
+// Loading the EventEmitter constructor
+var EventEmitter = require('events').EventEmitter;
+
+//In this case, we want the logger to emit Events by adding a listener
+var logger = new EventEmitter();
+
+// This is listening to the error event, and executing the callback function
+logger.on('error', function(message){
+    console.log('ERR: '+message);
+});
+
+// The following code triggers the 'error' event
+logger.emit('error', 'This is the first error');
+logger.emit('error', 'This is the second error');
+
+
+/* Run the following cmd to start node and run the server
+ node '2 - Events.js'
+
+You should see the following output:
+
+ ERR: This is the first error
+ ERR: This is the second error
+
+ */
+
