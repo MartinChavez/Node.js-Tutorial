51c4bb9063de873b5095a7e8dd15ab4ad8bd0bda
MartinChavez <info@martinchavezaguilar.com>
2015-11-22 14:27:24 -0800

    finish with content for npm chapter

--- 10 - Install NPM Modules.js
+++ 10 - Install NPM Modules.js
@@ -18,3 +18,18 @@
 
 // Tip: you can search modules from command line by using:
 // 'npm search <module>'
+
+// Best Practice: When creating a node project, you should create a package.json file where you can specify
+// multiple options (name, version, etc), but the most important aspect, is to specify the dependencies,in other words,
+// the modules that your application needs to run.
+
+// Once the dependencies have been specified, you can verify that they are installed by running:
+// 'npm install'
+// This will look inside the package.json file and fetch any missing dependencies
+
+// When you work on modules that were fetched from npm, it is not going to have the node_modules directory with all
+// the dependencies, you need to run 'npm install' in order to create it.
+
+// It is worth noticing that the modules that you app uses, may contain a package.json that references other modules
+// 'npm install' will install all of the dependencies and create a new node_modules directory
+
