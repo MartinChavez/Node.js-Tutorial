02b5ca7d53ef818340cadef564df4b309a127c50
MartinChavez <info@martinchavezaguilar.com>
2015-11-22 13:40:39 -0800

    Fix comments on Module Loader

--- 8 - Module Loader.js
+++ 8 - Module Loader.js
@@ -3,11 +3,18 @@ var moduleFunction = require('./7 - modules');
 // It is possible to use all of the export methods from the custom Module
 moduleFunction();
 
-
+// When you require a module using './', it is going to look in the same directory
+// as the application, for a file with that name
 var makeRequest = require('./9 - http module');
 makeRequest('http req 1');
 makeRequest('http req 2');
 
+// When you use '../', it is going to look in the parent directory
+
+// When you don't specify any directory, it is going to search in node_modules directories (inside the current app)
+
+// When require doesn't find the module in the current app, it is going to search
+// in node_modules directory (Home directory)
 
 /*
 Run the following cmd to start node and load the custom module
@@ -16,5 +23,6 @@ Run the following cmd to start node and load the custom module
 
 Expected response:
 
- - "Module loaded" on cmd
+ - "Module loaded" from '7 - Modules'
+ - Two HTTP requests from '9 - Http Module'
 */
