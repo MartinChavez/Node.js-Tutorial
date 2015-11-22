f02a145d855009257aa2a3d74c1105b933d2ff88
MartinChavez <info@martinchavezaguilar.com>
2015-11-22 14:06:19 -0800

    Add npm modules chapter

---
+++ 10 - Install NPM Modules.js
@@ -0,0 +1,20 @@
+/* npm */
+/*
+- Package manager for Node.js
+- Installed with Node.js
+- Has a Module repository (npmjs.org)
+- Dependency Management built-in
+- Easy to publich modules
+*/
+
+//Install 'request' package by running:
+// 'npm install request'
+// This will install the module inside the 'node_modules' directory
+
+// Install modules with executables globally with the 'g' flag
+// 'npm install typescript -g'
+
+// Note: Global npm modules can't be required, you still need to install locally
+
+// Tip: you can search modules from command line by using:
+// 'npm search <module>'
