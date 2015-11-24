1d20caf229841c5f463b8c34ae61a76cd302eec6
MartinChavez <info@martinchavezaguilar.com>
2015-11-24 04:13:03 -0800

    Fix comments for npm Modules

--- 10 - Install NPM Modules.js
+++ 10 - Install NPM Modules.js
@@ -4,10 +4,10 @@
 - Installed with Node.js
 - Has a Module repository (npmjs.org)
 - Dependency Management built-in
-- Easy to publich modules
+- Easy to publish modules
 */
 
-//Install 'request' package by running:
+// Install 'request' package by running:
 // 'npm install request'
 // This will install the module inside the 'node_modules' directory
 
@@ -21,7 +21,7 @@
 
 // Best Practice: When creating a node project, you should create a package.json file where you can specify
 // multiple options (name, version, etc), but the most important aspect, is to specify the dependencies,in other words,
-// the modules that your application needs to run.
+// the modules that your application needs in order to be able to run.
 
 // Once the dependencies have been specified, you can verify that they are installed by running:
 // 'npm install'
@@ -30,5 +30,5 @@
 // When you work on modules that were fetched from npm, it is not going to have the node_modules directory with all
 // the dependencies, you need to run 'npm install' in order to create it.
 
-// It is worth noticing that the modules that you app uses, may contain a package.json that references other modules
+// It is worth noticing that the modules that your app uses, may contain a package.json that references other modules.
 // 'npm install' will install all of the dependencies and create a new node_modules directory
