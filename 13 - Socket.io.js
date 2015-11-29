74b63a4229691076cbc56e03c6975df48a814b2f
MartinChavez <info@martinchavezaguilar.com>
2015-11-29 15:10:31 -0800

    Finish comments for socket.io and example html file

--- 13 - Socket.io.js
+++ 13 - Socket.io.js
@@ -12,3 +12,34 @@
 Install by running:
 npm install --save socket.io
 */
+var express = require('express');
+var app = express();
+// Create an http server and dispatch the requests to express
+var server = require('http').createServer(app);
+// Load the socket.io module and allow it to use the http server to listen for requests
+var io = require('socket.io')(server);
+
+// Listen for connection events (on socket.io) and define the callback function
+io.on('connection', function(client){
+    console.log('Client connected...');
+    // Emits the 'messages' event on the clients and sends the object {socket:io}
+    client.emit('messages', {socket:'i.o'})
+})
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/socket.html');
+});
+
+server.listen(8080);
+
+/*
+Run the following cmd to start node and create the scoket.io server:
+ node '.\13 - Socket.io.js'
+
+- See socket.html for client-side code
+
+On your browser: localhost:8080
+
+Expected response:
+ - console.log with "i.o"
+ */
