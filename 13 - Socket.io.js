dcadd36fda58f8f5e4701bbbbec85b589a7f417b
MartinChavez <info@martinchavezaguilar.com>
2015-11-29 13:36:40 -0800

    Add socket.io

---
+++ 13 - Socket.io.js
@@ -0,0 +1,14 @@
+/* Web Sockets */
+/*
+- Web sockets allow the creation of connections between clients and server.
+- On these connections data can flow (back adn forward) in real time
+*/
+
+/* Socket.io */
+/*
+- Great tool for real-time applications
+- Provides fall-backs in case the client doesn't support web sockets
+
+Install by running:
+npm install --save socket.io
+*/
