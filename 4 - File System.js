ee382f3dc2b91761ecc37875c5a972fbe8144252
MartinChavez <info@martinchavezaguilar.com>
2015-12-01 20:14:05 -0800

    Format document and finish content

--- 4 - File System.js
+++ 4 - File System.js
@@ -1,15 +1,17 @@
 /*
-File System (using streams)
+ File System (using streams)
 
-Reading from a file in the File system using streams is easy with Node.js, follow this example:
-*/
+ Reading from a file in the File system using streams is easy with Node.js, follow this example:
+ */
 // Require the filesystem module
 var fs = require('fs');
 // Create a 'read' stream from the original file
-var readFile =  fs.createReadStream("README.md");
+var readFile = fs.createReadStream("README.md");
 // Create a 'write' stream to the destination file
-var writeFile =  fs.createWriteStream("README_copy.md");
-// Streaming is simple with the pipe function
+var writeFile = fs.createWriteStream("README_copy.md");
+// Streaming is simple with the 'pipe' function
+// The pipe function pulls all the data out of a readable stream, and writes it to the supplied destination,
+// automatically managing the flow so that the destination is not overwhelmed by a fast readable stream
 readFile.pipe(writeFile);
 
 /*
